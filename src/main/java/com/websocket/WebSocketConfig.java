a3a28fcc03db719469fcaef4db844b6cace7443e
orca <hee1su123@naver.com>
2022-07-01 21:32:41 +0900

    first commit

---
+++ src/main/java/com/websocket/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@EnableWebSocket // 웹소켓 활성화
+@Configuration // 이게 없으니까 왜 안되지...? 이건 @Bean 을 등록할 때 사용되는 어노테이션인데..
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
